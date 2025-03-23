6e2b14e6b8a5d3335072a4d2ba3e8b4102bda859
INankova <ivana.nankova@gmial.com>
2025-03-23 17:53:59 +0200

    Implemented CartRepository, CartService and CheckoutService

---
+++ src/main/java/com/example/event_management_system/Cart/repository/CartRepository.java
@@ -0,0 +1,14 @@
+package com.example.event_management_system.Cart.repository;
+
+import com.example.event_management_system.Cart.model.Cart;
+import com.example.event_management_system.User.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findByUserId(UUID userId);
+
+    Cart findByUser(User user);
+}
---
+++ src/main/java/com/example/event_management_system/Cart/service/CheckoutService.java
@@ -0,0 +1,56 @@
+package com.example.event_management_system.Cart.service;
+
+import com.example.event_management_system.Cart.model.Cart;
+import com.example.event_management_system.Cart.model.CartItem;
+import com.example.event_management_system.Cart.repository.CartRepository;
+import com.example.event_management_system.Event.model.Event;
+import com.example.event_management_system.Event.repository.EventRepository;
+import com.example.event_management_system.Ticket.model.Ticket;
+import com.example.event_management_system.Ticket.model.TicketStatus;
+import com.example.event_management_system.Ticket.repository.TicketRepository;
+import com.example.event_management_system.User.model.User;
+import com.example.event_management_system.User.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+public class CheckoutService {
+    private final CartRepository cartRepository;
+    private final TicketRepository ticketRepository;
+    private final EventRepository eventRepository;
+    private final UserRepository userRepository;
+
+    public CheckoutService(CartRepository cartRepository, TicketRepository ticketRepository, EventRepository eventRepository, UserRepository userRepository) {
+        this.cartRepository = cartRepository;
+        this.ticketRepository = ticketRepository;
+        this.eventRepository = eventRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    public void processCheckout(UUID userId) {
+        User user = userRepository.findById(userId).get();
+        Cart cart = cartRepository.findByUser(user);
+
+        for (CartItem item : cart.getItems()) {
+            Event event = eventRepository.findById(item.getEvent().getId()).get();
+
+            // Създаване на билети
+            for (int i = 0; i < item.getQuantity(); i++) {
+                Ticket ticket = new Ticket();
+                ticket.setOwner(user);
+                ticket.setEvent(event);
+                ticket.setPrice(event.getPrice());
+                ticket.setStatus(TicketStatus.ACTIVE);
+                ticketRepository.save(ticket);
+            }
+        }
+
+        // Clear cart
+        cart.getItems().clear();
+        cartRepository.save(cart);
+    }
+}
