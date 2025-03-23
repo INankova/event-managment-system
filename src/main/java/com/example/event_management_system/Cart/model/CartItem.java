d4b36363671d15fb12aa5ad74c90d8e369fcef03
INankova <ivana.nankova@gmial.com>
2025-03-23 17:52:35 +0200

    Added Cart and CartItem

---
+++ src/main/java/com/example/event_management_system/Cart/model/CartItem.java
@@ -0,0 +1,36 @@
+package com.example.event_management_system.Cart.model;
+
+import com.example.event_management_system.Event.model.Event;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    private Event event;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    private Cart cart;
+
+    public CartItem(Event event, int quantity) {
+        this.event = event;
+        this.quantity = quantity;
+    }
+}
