9a08566f354e07c451d09af645db9ac5ef45d30c
INankova <ivana.nankova@gmial.com>
2025-11-06 20:27:54 +0200

    fix: fix form binding and update CSS styles

--- src/main/java/com/example/event_management_system/web/dto/RegisterRequest.java
+++ src/main/java/com/example/event_management_system/web/dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.example.event_management_system.web.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -18,7 +19,7 @@ public class RegisterRequest {
     @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters!")
     private String username;
 
-    @NotNull(message = "Email must not be empty!")
+    @NotBlank(message = "Email must not be empty!")
     @Email(message = "Email must not be empty!")
     private String email;
 
